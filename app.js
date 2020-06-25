135ed64d2cb9d62287762e59e9a80b3dc355c5c4
rizal <you@example.com>
2020-06-25 14:31:40 +0700

    run API with data

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -32,7 +29,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+});
+
+app.listen(3000, () => {
+  console.log(`web ini berjalan di port 3000!`);
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const fs = require('fs');
+const router = express.Router();
+
+const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+const writeData = (data) => fs.writeFileSync('./data.json', JSON.stringify(data, null, 2));
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.json({ data });
 });
 
 module.exports = router;
